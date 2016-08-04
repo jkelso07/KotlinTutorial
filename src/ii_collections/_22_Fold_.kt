a0c7b17f020cd2227679d4a4e93982af00476b73
Josh Kelso <josh.kelso@instil.co>
2016-08-04 09:00:23 +0100

    Add tests and adjust example methods accordingly

--- src/ii_collections/_22_Fold_.kt
+++ src/ii_collections/_22_Fold_.kt
@@ -1,14 +1,14 @@
 package ii_collections
 
-fun example9() {
+fun example9(): Int {
     val result = listOf(1, 2, 3, 4).fold(1, { partResult, element -> element * partResult })
-    result == 24
+    return result
 }
 
 // The same as
 fun whatFoldDoes(): Int {
     var result = 1
-    listOf(1, 2, 3, 4).forEach { element -> result = element * result}
+    listOf(1, 2, 3, 4).forEach { element -> result *= element}
     return result
 }
 
--- test/ii_collections/_22_Fold.kt
+++ test/ii_collections/_22_Fold.kt
@@ -9,6 +9,15 @@ import org.junit.Test
 
 
 class _22_Fold {
+    @Test fun shouldReturn24(){
+        assertEquals(24, example9())
+    }
+    @Test fun shouldAlsoReturn24(){
+        assertEquals(24, whatFoldDoes())
+    }
+    @Test fun shouldProduceSameAnswer(){
+        assertEquals(example9(), whatFoldDoes())
+    }
     @Test fun testGetProductsOrderedByAllCustomers() {
         val testShop = shop("test shop for 'fold'") {
             customer(lucas, Canberra) {
