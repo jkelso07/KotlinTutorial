bc1d9d56c4d6e32414db50aa4675ac3887fe0770
Josh Kelso <josh.kelso@instil.co>
2016-08-04 09:45:04 +0100

    Add my solution to part 23

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -1,18 +1,23 @@
 package ii_collections
 
+import ii_collections.data.orderedProducts
+
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter { it.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    val deliveredOrders = orders.filter { it.isDelivered }
+    return deliveredOrders.flatMap { it.products }.toSet().maxBy { it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    val isProduct: (Product) -> Boolean = { it == product }
+    customers.flatMap { it.orders.flatMap { it.products } }.count(isProduct)
+    return customers.flatMap { it.orders.flatMap { it.products } }.count(isProduct)
 }
